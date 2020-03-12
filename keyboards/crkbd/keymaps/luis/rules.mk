1a64e6d94bbaf57a48af63a716f14c80f9dd3c37
alpz <luislpz978@gmail.com>
2020-03-12 03:23:33 -0600

    Enable media keys

--- keyboards/crkbd/keymaps/luis/rules.mk
+++ keyboards/crkbd/keymaps/luis/rules.mk
@@ -1,4 +1,6 @@
 
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
         ./lib/rgb_state_reader.c \
